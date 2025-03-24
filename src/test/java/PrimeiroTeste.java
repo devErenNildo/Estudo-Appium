52ec28706a2848843066dff3d90df603a5fd7ec0
eren <dev.erenildo@gmail.com>
2025-03-24 11:20:19 -0300

    teste calculadora soma

--- src/test/java/PrimeiroTeste.java
+++ src/test/java/PrimeiroTeste.java
@@ -1,6 +1,8 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,15 +18,22 @@ public class PrimeiroTeste {
     public void setUp() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
-        caps.setCapability("deviceName", "emulator-5554");
-        caps.setCapability("app", System.getProperty("user.dir") + "/apps/ApiDemos.apk");
-        drive = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("platformVersion", "16");
+        caps.setCapability("deviceName", "Pixel 9");
+        caps.setCapability("appPackage", "com.google.android.calculator");
+        caps.setCapability("appActivity", "com.android.calculator2.Calculator");
+        drive = new AndroidDriver(new URL("http://localhost:4723"), caps);
 
     }
 
     @Test
-    public void teste(){
-        drive.findElementByAccessibilityId("App").click();
+    public void testSomar(){
+        drive.findElement(By.id("com.google.android.calculator:id/digit_1")).click();
+        drive.findElement(By.id("com.google.android.calculator:id/op_add")).click();
+        drive.findElement(By.id("com.google.android.calculator:id/digit_9")).click();
+
+        Assert.assertEquals(drive.findElement(By.id("com.google.android.calculator:id/result_preview")).getText(), "10");
     }
 
     @AfterTest
